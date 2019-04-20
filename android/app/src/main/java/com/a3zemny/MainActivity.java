4da2acf23c63064c73709486161784cb239b6537
Mohammed Aljefri <algfry12@gmail.com>
2019-04-20 12:27:15 +0300

    first commit

---
+++ android/app/src/main/java/com/a3zemny/MainActivity.java
@@ -0,0 +1,15 @@
+package com.a3zemny;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "A3zemny";
+    }
+}
